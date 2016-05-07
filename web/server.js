158457bd1b579dfe77c7191b8b344606a4074967
snkenjoi <snkenjoi@gmail.com>
2016-05-07 04:51:56 +0100

    Sat May  7 04:51:56 BST 2016

--- web/server.js
+++ web/server.js
@@ -1,24 +1,62 @@
 var url = require("url");
 var http = require("http");
+var express = require('express');
+var app = express();
 
 module.exports = function(client, db, config) {
 
     // http://kirjava.xyz:8888/?say=1&users=%238bitvape&message=test
 
-    var server = http.createServer((req, res) => {
+    // var server = http.createServer((req, res) => {
+    //     req = url.parse(req.url, true);
+
+    //     if(req.query.users && req.query.message) {
+    //         res.end('sent ' + req.query.message + ' to ' + req.query.users);
+
+    //         var action = req.query.say ? 'say' : 'notice';
+
+    //         req.query.users.split(',').forEach(d => {
+    //             client[action](d, req.query.message);
+    //         })
+    //     }
+
+    // });
+
+    // server.listen(config.serverport, () => console.log('notify server listening'))
+
+    app.get('/api/say', (req,res) => {
         req = url.parse(req.url, true);
 
-        if(req.query.users && req.query.message) {
-            res.end('sent ' + req.query.message + ' to ' + req.query.users);
+        if(req.query.user && req.query.message) {
+            res.send('sent message ' + req.query.message + ' to ' + req.query.user);
 
-            var action = req.query.say ? 'say' : 'notice';
-
-            req.query.users.split(',').forEach(d => {
-                client[action](d, req.query.message);
+            req.query.user.split(',').forEach(d => {
+                client.say(d, req.query.message);
             })
         }
+    })
 
+    app.get('/api/notice', (req,res) => {
+        req = url.parse(req.url, true);
+
+        if(req.query.user && req.query.message) {
+            res.send('sent notice ' + req.query.message + ' to ' + req.query.user);
+
+            req.query.user.split(',').forEach(d => {
+                client.notice(d, req.query.message);
+            })
+        }
+    })
+
+
+
+    app.get('/', function (req, res) {
+      res.send('Hello World!');
     });
 
-    server.listen(config.serverport, () => console.log('notify server listening'))
+    app.listen(config.serverport, function () {
+        console.log('Web server listening on ' + config.serverport);
+    });
+
+
 }

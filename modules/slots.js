18752a753668b7484cddda76ed8d82a4c933ad92
snkenjoi <snkenjoi@gmail.com>
2016-07-05 20:06:01 +0100

    Tue Jul  5 20:06:01 BST 2016

--- modules/slots.js
+++ modules/slots.js
@@ -5,14 +5,21 @@ var client;
 
 var jackpot = 100;
 
+var timers = {};
+
 var slots = {
     init(_client) { client = _client },
     go(user) {
 
+        if (timers[user]) {
+            client.say(config.channel, 'please wait');
+            return;
+        }
+
         var sym = ['1,8 BELL ','1,4 CHERRY ','1,6 PLUM ','1,9 WEED ','0,4 STRAWBERRY ','0,1 COAL ','1,11 BLUEBERRY ','1,7 ORANGE ','4,3 APPLE '];
 
 
-        var msg = `[7SLOT-MACHINE] Current Jackpot: €${jackpot}  :::  Rolling the wheels.\n`;
+        var msg = `[7SLOT-MACHINE] Current Jackpot: ¥${jackpot}  :::  Rolling the wheels.\n`;
 
         function get() {
             return sym[(Math.random()*9)|0];
@@ -24,13 +31,13 @@ var slots = {
 
         if (rslt[0]==rslt[1]&&rslt[0]==rslt[2]) {
             addPoints(user, jackpot);
-            msg += `0 1,8 J 1,4 A 1,9 C 1,7 K 1,13 P 1,11 O 1,5 T    !!! ${user} just won €${jackpot} !\n`
+            msg += `0 1,8 J 1,4 A 1,9 C 1,7 K 1,13 P 1,11 O 1,5 T    !!! ${user} just won ¥${jackpot} !\n`
 
             jackpot = 100;
         }
         else if (rslt[0]==rslt[1]||rslt[1]==rslt[2]) {
             var win = (Math.random()*4)|0;
-            msg += `!!! ${user} just won €${win+1} !\n`
+            msg += `!!! ${user} just won ¥${win+1} !\n`
             addPoints(user, win);
         }
         else {
@@ -39,6 +46,11 @@ var slots = {
         }
 
         client.say(config.channel, msg);
+
+        timers[user] = setTimeout(() => {
+            timers[user] = void 0;
+        }, 60000);
+
     },
     stats() {
         db.all('select username,slots from points',
@@ -59,12 +71,12 @@ function addPoints(from, points) {
         (e,r) => {
             if (r == undefined) {
                 db.run('INSERT INTO points(username,slots) VALUES (?,?)', [from, 100 + points]);
-                client.say(config.channel, `${from} has €${100 + points} `);
+                client.say(config.channel, `${from} has ¥${100 + points} `);
             }
             else {
                 var pts = r['slots']?points + r['slots']:points+100;
                 db.run('UPDATE points SET slots = ? WHERE username = ?', [pts, from]);
-                client.say(config.channel, `${from} has €${pts} `);
+                client.say(config.channel, `${from} has ¥${pts} `);
             }
 
         });

f5a255d68aa365a5233d3302fab0b973bb3ccb3c
snkenjoi <snkenjoi@gmail.com>
2016-07-07 22:10:17 +0100

    Thu Jul  7 22:10:17 BST 2016

--- modules/slots.js
+++ modules/slots.js
@@ -32,7 +32,7 @@ var slots = {
             return;
         }
 
-        var sym = ['1,8 BELL ','1,4 CHERRY ','1,6 PLUM ','1,9 WEED ','0,4 STRAWBERRY ','0,1 COAL ','1,11 BLUEBERRY ','1,7 ORANGE ','4,3 APPLE ','0,7 WOOD ','15,8 LEMON ','1,16 BANANA ','0,6 PURPLE ','5,3 WATERMELON ', '1,16 ' + c.rainbow(' SKITTLES ') + '1,16 '];
+        var sym = ['1,8 BELL ','1,4 CHERRY ','1,6 PLUM ','1,9 WEED ','0,4 STRAWBERRY ','0,1 COAL ','1,11 BLUEBERRY ','1,7 ORANGE ','4,3 APPLE ','0,7 WOOD ','15,8 LEMON ','1,16 BANANA ','0,6 PURPLE ','5,3 WATERMELON ', c.rainbow.bgwhite(' SKITTLES ')];
 
 
         var msg = `[7SLOT-MACHINE] Current Jackpot: €${jackpot}  :::  Rolling the wheels.\n`;

4e3115782878f64fdf71b8867a702a6566d33451
snkenjoi <snkenjoi@gmail.com>
2016-07-07 22:06:29 +0100

    Thu Jul  7 22:06:29 BST 2016

--- modules/slots.js
+++ modules/slots.js
@@ -1,6 +1,7 @@
 var irc = require('irc');
 var config = require('../index').config;
 var db = require('../index').db;
+var c = require('irc-colors');
 var client;
 
 var jackpot = 100;
@@ -31,7 +32,7 @@ var slots = {
             return;
         }
 
-        var sym = ['1,8 BELL ','1,4 CHERRY ','1,6 PLUM ','1,9 WEED ','0,4 STRAWBERRY ','0,1 COAL ','1,11 BLUEBERRY ','1,7 ORANGE ','4,3 APPLE ','0,7 WOOD ','15,8 LEMON ','1,16 BANANA ','0,6 PURPLE ','5,3 WATERMELON '];
+        var sym = ['1,8 BELL ','1,4 CHERRY ','1,6 PLUM ','1,9 WEED ','0,4 STRAWBERRY ','0,1 COAL ','1,11 BLUEBERRY ','1,7 ORANGE ','4,3 APPLE ','0,7 WOOD ','15,8 LEMON ','1,16 BANANA ','0,6 PURPLE ','5,3 WATERMELON ', c.rainbow('ＳＫＩＴＴＬＥＳ')];
 
 
         var msg = `[7SLOT-MACHINE] Current Jackpot: €${jackpot}  :::  Rolling the wheels.\n`;
